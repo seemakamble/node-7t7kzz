bbd0e1905285fa1866d9f5bffe2e873488334716
seemakamble <98740402+seemakamble@users.noreply.github.com>
2023-01-06 11:21:00 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-7t7kzz
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-7t7kzz)
---
+++ index.js
@@ -0,0 +1,33 @@
+// run `node index.js` in the terminal
+const http = require('http');
+const nodemailer = require('nodemailer');
+console.log(`Hello Node.js v${process.versions.node}!`);
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'nareshsit7@gmail.com',
+    pass: 'f1gfhgm23nbcfgfdgvx54352',
+  },
+});
+const options = {
+  from: 'nareshsit7@gmail.com',
+  to: 'nareshbjava7@gmail.com',
+  subject: 'Testing mail from express',
+  text: 'This is text mail',
+};
+
+http
+  .createServer((req, res) => {
+    res.write('hellooo');
+    transporter.sendMail(options, (err, info) => {
+      if (err) {
+        console.log(err);
+        res.write('Error');
+      } else {
+        res.write('mail is sent');
+      }
+      res.end();
+    });
+    // res.end();
+  })
+  .listen(4040, () => console.log('server is running....'));
